ebf49e7ade276c977eea90ed0447854d690dab2d
lombosmonika <lombos.monika@gmail.com>
2023-11-21 13:50:26 +0100

    chore: justify view

--- app/src/main/java/com/example/xpense_tracker/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/xpense_tracker/ui/notifications/NotificationsFragment.java
@@ -18,14 +18,10 @@ public class NotificationsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
